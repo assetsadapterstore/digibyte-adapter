df6fee25779153fccee87cfce0c58a3ebf2510fb
chance <zhiquan911@qq.com>
2019-11-18 17:56:55 +0800

    SupportBlockchainDAI

--- openwtester/subscribe_test.go
+++ openwtester/subscribe_test.go
@@ -16,6 +16,7 @@
 package openwtester
 
 import (
+	"github.com/blocktree/openwallet/common/file"
 	"path/filepath"
 	"testing"
 
@@ -96,7 +97,19 @@ func TestSubscribeAddress(t *testing.T) {
 
 	//log.Debug("already got scanner:", assetsMgr)
 	scanner := assetsMgr.GetBlockScanner()
-	scanner.SetRescanBlockHeight(8813250)
+	if scanner.SupportBlockchainDAI() {
+		dbFilePath := filepath.Join("data", "db")
+		dbFileName := "blockchain.db"
+		file.MkdirAll(dbFilePath)
+		dai, err := openwallet.NewBlockchainLocal(filepath.Join(dbFilePath, dbFileName), false)
+		if err != nil {
+			log.Error("NewBlockchainLocal err: %v", err)
+			return
+		}
+
+		scanner.SetBlockchainDAI(dai)
+	}
+	//scanner.SetRescanBlockHeight(8813250)
 
 	if scanner == nil {
 		log.Error(symbol, "is not support block scan")
